a3d2445b490f1f4666406931b77de6102531eda8
c2096299 <sunilsingh_panwar@apple.com>
2026-03-30 16:04:13 +0530

    remove the hardcoded valuess

--- js/config.js
+++ js/config.js
@@ -12,12 +12,11 @@
 const CONFIG = {
     // Google Apps Script Web App URL
     // Get this after deploying your Google Apps Script
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwvjw5yxcFgGk1h3g8aCwm3w24WdJROcU8qkHlZCbLho1C6zFNv8r6S1SazRlCS4KdJFw/exec',
+    GOOGLE_SCRIPT_URL: 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE',
     
     // reCAPTCHA Site Key (Public key - safe to expose)
     // Get from: https://www.google.com/recaptcha/admin
     // Leave empty ('') to disable reCAPTCHA
-    // TODO: Add your reCAPTCHA Site Key here if you want to enable it
     RECAPTCHA_SITE_KEY: '',
     
     // Enable/disable reCAPTCHA
